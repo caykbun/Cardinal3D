09f93739a802e74667c80635de12e1732ad58ea1
caykbun <christineeee1220@gmail.com>
2024-02-24 23:47:45 -0800

    Task 2 EC: intersect sphere

--- src/student/shapes.cpp
+++ src/student/shapes.cpp
@@ -26,11 +26,48 @@ Trace Sphere::hit(const Ray& ray) const {
     // return that one!
 
     Trace ret;
+    ret.hit = false;
     ret.origin = ray.point;
-    ret.hit = false;       // was there an intersection?
-    ret.distance = 0.0f;   // at what distance did the intersection occur?
-    ret.position = Vec3{}; // where was the intersection?
-    ret.normal = Vec3{};   // what was the surface normal at the intersection?
+
+    if (radius <= 0) {
+        return ret;
+    }
+
+    // Note: No center information of the sphere, seems like ray will be transformed 
+    // so that the sphere is centered at the origin
+
+    // b^2 - 4ac = 4(o \dot d)^2 - 4|d|^2(|o|^2 - r)
+    float a = pow(ray.dir.norm(), 2);
+    float b = 2 * dot(ray.point, ray.dir);
+    float c = pow(ray.point.norm(), 2) - radius;
+    float test = b * b - 4 * a * c;
+
+    if (test < 0) {
+        return ret;
+    }
+
+    // solve t by the quadratic formula
+    float t1 = (-b + sqrt(test)) / (2 * a);
+    float t2 = (-b - sqrt(test)) / (2 * a);
+    if (t1 > t2) std::swap(t1, t2);
+
+    float intersection_t = -1;
+    if (t1 >= ray.dist_bounds.x && t1 <= ray.dist_bounds.y) {
+        intersection_t = t1;
+    } else if (t2 != t1 && t2 >= ray.dist_bounds.x && t2 <= ray.dist_bounds.y) {
+        intersection_t = t2;
+    }
+
+    // printf("sphere: %.3f\n", intersection_t);
+
+    if (intersection_t >= 0) {
+        ret.hit = true;
+        ret.distance = intersection_t;
+        ret.position = ray.point + ret.distance * ray.dir;
+        ret.normal = ret.position;  // normal at the intersection point is the vector from the origin to the point
+        ray.dist_bounds.y = ret.distance;
+    }
+
     return ret;
 }
 

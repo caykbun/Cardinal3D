4d86f8d7ed1bd580d9ff9ecbe99c873a95f099fc
angli66 <ang6li98@gmail.com>
2024-02-25 18:30:01 -0800

    Minor fix

--- src/student/camera.cpp
+++ src/student/camera.cpp
@@ -19,7 +19,10 @@ Ray Camera::generate_ray(Vec2 screen_coord) const {
     // the camera space to world space transform (iview) to transform the ray back into world space.
 
     // compute the width and height of the sensor plane
-    Vec2 up_right = Vec2 {aspect_ratio * tan(vert_fov * PI_F / 360), tan(vert_fov * PI_F / 360)};
+    Vec2 up_right = Vec2 {
+        aspect_ratio * static_cast<float>(tan(vert_fov * PI_F / 360)),
+        static_cast<float>(tan(vert_fov * PI_F / 360))
+    };
     Vec2 point_on_sensor = -1.f * up_right +  2 * up_right * screen_coord;
     Vec3 o = (iview * Vec4(0.f, 0.f, 0.f, 1.f)).project();
     Vec3 d = (iview * Vec4(point_on_sensor.x, point_on_sensor.y, -1.f, 1.f)).project() - o;
